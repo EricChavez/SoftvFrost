9b9b1d674f5a0659a7703283d3440efdd73ee65e
EricChavez <erictics@gmail.com>
2018-06-15 10:08:21 -0500

    manuales

--- app/scripts/globalService.js
+++ app/scripts/globalService.js
@@ -48,37 +48,37 @@ angular.module('softvFrostApp')
  */
     
 
-/* 
-    //rutas locales
+
+   /*  //rutas locales
      svc.getUrl = function () {  
        //return  'http://localhost:64481/SoftvWCFService.svc'
-        return 'http://192.168.50.33:3000/SoftvWCFService.svc';
+        return 'http://192.168.50.200:3000/SoftvWCFService.svc';
      //return 'http://172.16.126.44:8089/SoftvWCFService.svc';
    
      };
 
      svc.getUrlReportes = function () {
-      return 'http://192.168.50.33:3000';
+      return 'http://192.168.50.200:3000';
 
    };
 
      svc.getUrlmemoriatecnica = function () {
-      return 'http://192.168.50.33:3500/SoftvWCFService.svc';
+      return 'http://192.168.50.200:3500/SoftvWCFService.svc';
     // return 'http://localhost:64482/SoftvWCFService.svc';
     };
 
      svc.getUrlmemoriatecnicareportes = function () {
-       return 'http://192.168.50.33:3500';
+       return 'http://192.168.50.200:3500';
        //   return 'http://localhost:64482';
      };
 
     svc.getUrlmemoriatecnicaImages = function () {
-     return 'http://192.168.50.33:3500/MemoriaTecnica';
+     return 'http://192.168.50.200:3500/MemoriaTecnica';
    //  return 'http://localhost:64482/MemoriaTecnica';
    };
 
      svc.getUrlHughesService = function () {//SoftvForstAPI local
-      return 'http://192.168.50.33:3000/BossAPI/SoftvFrostAPI';
+      return 'http://192.168.50.200:3000/BossAPI/SoftvFrostAPI';
       //return 'http://189.254.231.35:9092/SoftvFrostAPI';
       //return "http://localhost:21590/SoftvFrostAPI";
     };
@@ -88,14 +88,14 @@ angular.module('softvFrostApp')
      };
 
     svc.getUrlHughesMonitoreo = function () {
-     return 'http://192.168.50.33:3000/BossMonitoreo/api';
+     return 'http://192.168.50.200:3000/BossMonitoreo/api';
      };
 
      svc.getType = function () {
        return 'TEV';
      };    
+  */
  
- */
 
        //rutas server de pruebas
     svc.getUrl = function () {
@@ -137,7 +137,7 @@ angular.module('softvFrostApp')
 
     svc.getType = function () {
       return 'TLV';
-    }; 
+    };  
  
 
  
